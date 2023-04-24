d5f8a50ff99c1d8af38d16c44cd4ca629c81fb9e
Maurice J Niessen <post@mjniessen.com>
2023-04-24 13:01:03 +0200

    emoji and unicode tests added

---
+++ uniemoji-test.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : Maurice-Jörg Nießen (post@mjniessen.com)
+# @file        : smoke-test
+# @created     : Saturday Apr 22, 2023 08:11:09 CEST
+#
+# @description : Test font settings
+######################################################################
+
+# https://emojipedia.org/emoji-11.0/
+echo
+echo "\033[4mEmoji Version 11.0 (2018)\033[24m"
+echo " Woozy Face         : 🥴"
+echo " Tooth              : 🦷"
+echo " Cupcake            : 🧁" 
+echo " Teddy Bear         : 🧸"
+echo " Petri Dish         : 🧫"
+echo " Roll of Paper      : 🧻"
+
+
+# https://emojipedia.org/emoji-12.0/
+echo
+echo "\033[4mEmoji Version 12.0 (2019)\033[24m"
+echo " Yawning Face       : 🥱"
+echo " Mechanical Arm     : 🦾"
+echo " Flamingo           : 🦩" 
+echo " Safety Vest        : 🦺"
+echo " Garlic             : 🧄"
+echo " Stethoscope        : 🩺"
+
+
+# https://emojipedia.org/emoji-13.0/
+echo
+echo "\033[4mEmoji Version 13.0 (2020)\033[24m"
+echo " Teapot             : 🫖"
+echo " Bubble Tea         : 🧋"
+echo " Srewdriver         : 🪛" 
+echo " Piñata             : 🪅"
+echo " Worm               : 🪱"
+echo " Transgender Symbol : ⚧️"
+
+
+# https://emojipedia.org/emoji-14.0/
+echo
+echo "\033[4mEmoji Version 14.0 (2021)\033[24m"
+echo " Dotted Line Face   : 🫥"
+echo " Bubbles            : 🫧"
+echo " Biting Lip         : 🫦"
+echo " Meelting Face      : 🫠"
+echo " X-Ray              : 🩻"
+echo " Mirror Ball        : 🪩"
+
+# https://emojipedia.org/emoji-15.0/
+echo
+echo "\033[4mEmoji Version 15.0 (2022)\033[24m"
+echo " Shaking Face       : 🫨"
+echo " Light Blue Heart   : 🩵"
+echo " Goose              : 🪿"
+echo " Hair Pick          : 🪮"
+echo " Wireless           : 🛜"
+
+
+
+

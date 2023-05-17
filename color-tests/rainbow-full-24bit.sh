e316c8eeaba19f5994db2571d03b1558fcacea0d
Maurice J Niessen <post@mjniessen.com>
2023-05-17 09:10:28 +0200

    full screen rainbow added

---
+++ color-tests/rainbow-full-24bit.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Purpose: Show a rainbow in a single line to test 24 bit color terminal support (of kitty).
+#          You should see smooth color transitions or even no transitions at all. The higher
+#          the width of the terminal, the smoother the transitions.
+#
+#  https://github.com/mjniessen/kitty/blob/main/color-tests/rainbow-24bit.sh
+#
+# Original idea and code can be found here: https://gist.github.com/XVilka/8346728
+# I just got rid of the awk stuff and bashified some things.
+# -- Maurice J Niessen 
+
+function rainbow {
+    local width
+    ((  lines=$(tput lines)-2, width=($(tput cols) * lines)-1 ))
+    local colnum
+    local r; local g; local b
+    for (( colnum=0; colnum<=width; colnum++ ))
+    do
+        (( r=255-(colnum*255/width) ))
+        (( g=(colnum*510/width) ))
+        (( b=(colnum*255/width) ))
+        (( g > 255 )) && (( g=510-g ))
+        printf "\033[48;2;%d;%d;%dm" $r $g $b
+        printf "\033[38;2;%d;%d;%dm" $(( 255-r )) $(( 255-g )) $(( 255-b ))
+        printf " \033[0m"
+    done
+}
+
+typeset -a colrs
+
+# Reset output, if something goes wrong
+trap 'tput sgr0; echo' EXIT
+
+rainbow
+
